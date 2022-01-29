706e64b974f1266a66c74bc95b55f9d2eef730d0
Felix Vemmer <felix.vemmer@gmail.com>
2022-01-29 12:12:03 +0100

    feat: completes challenge

--- packages/hardhat/contracts/Staker.sol
+++ packages/hardhat/contracts/Staker.sol
@@ -4,31 +4,68 @@ import "hardhat/console.sol";
 import "./ExampleExternalContract.sol";
 
 contract Staker {
+    ExampleExternalContract public exampleExternalContract;
 
-  ExampleExternalContract public exampleExternalContract;
+    constructor(address exampleExternalContractAddress) public {
+        exampleExternalContract = ExampleExternalContract(
+            exampleExternalContractAddress
+        );
+    }
 
-  constructor(address exampleExternalContractAddress) public {
-      exampleExternalContract = ExampleExternalContract(exampleExternalContractAddress);
-  }
+    // minimum required staking amount
+    uint256 public constant threshold = 1 ether;
 
-  // Collect funds in a payable `stake()` function and track individual `balances` with a mapping:
-  //  ( make sure to add a `Stake(address,uint256)` event and emit it for the frontend <List/> display )
+    // mapping that keeps track of staked balances
+    mapping(address => uint256) public balances;
 
+    // event for staked amounts
+    event Stake(address indexed sender, uint256 value);
 
-  // After some `deadline` allow anyone to call an `execute()` function
-  //  It should either call `exampleExternalContract.complete{value: address(this).balance}()` to send all the value
+    // staking payable function mapping each received stake to an address, requiring to stake at least 1 ether
+    function stake() public payable {
+        require(msg.value >= 0, "Stake amount must be positive");
+        balances[msg.sender] += msg.value;
+        emit Stake(msg.sender, msg.value);
+    }
 
+    // deadline until when the staker can withdraw his stake
+    uint256 public deadline = block.timestamp + 72 hours;
 
-  // if the `threshold` was not met, allow everyone to call a `withdraw()` function
+    modifier notCompleted() {
+        bool completed = exampleExternalContract.completed();
+        require(!completed, "Staking process already completed");
+        // Underscore is a special character only used inside
+        // a function modifier and it tells Solidity to
+        // execute the rest of the code.
+        _;
+    }
 
+    // function checking how much time is left
+    function timeLeft() public view returns (uint256) {
+        if (block.timestamp >= deadline) {
+            return 0;
+        }
+        return deadline - block.timestamp;
+    }
 
-  // Add a `withdraw(address payable)` function lets users withdraw their balance
+    function withdraw() public notCompleted {
+        // transfer the staked amount to the staker
+        payable(msg.sender).transfer(balances[msg.sender]);
 
+        // remove the staked amount from the mapping
+        balances[msg.sender] = 0;
+    }
 
-  // Add a `timeLeft()` view function that returns the time left before the deadline for the frontend
+    function execute() public notCompleted {
+        if (address(this).balance >= threshold && timeLeft() == 0) {
+            exampleExternalContract.complete{value: address(this).balance}();
+        } else {
+            bool openForWithdraw = true;
+            withdraw();
+        }
+    }
 
-
-  // Add the `receive()` special function that receives eth and calls stake()
-
-
+    receive() external payable notCompleted {
+        stake();
+    }
 }
