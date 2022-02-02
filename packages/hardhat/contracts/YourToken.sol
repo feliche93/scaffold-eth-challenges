ab319039215658f1693478ab141b5cf275c486a0
Felix Vemmer <felix.vemmer@gmail.com>
2022-02-02 13:37:22 +0100

    feat: creates token

--- packages/hardhat/contracts/YourToken.sol
+++ packages/hardhat/contracts/YourToken.sol
@@ -2,10 +2,11 @@ pragma solidity 0.8.4;
 // SPDX-License-Identifier: MIT
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
 // learn more: https://docs.openzeppelin.com/contracts/3.x/erc20
 
 contract YourToken is ERC20 {
-    constructor() ERC20("Gold", "GLD") {
-        //_mint( ~~~YOUR FRONTEND ADDRESS HERE~~~~ , 1000 * 10 ** 18);
+    constructor() ERC20("Felix", "FLX") {
+        _mint(0xe2f9a662DE63a5AB99F937fC82A93381262740c3, 1000 * 10**18);
     }
 }
