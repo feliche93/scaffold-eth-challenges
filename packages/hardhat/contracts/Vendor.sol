ed432b509a6ba24a7a60b94746be30a74eb6435c
Felix Vemmer <felix.vemmer@gmail.com>
2022-02-02 16:21:01 +0100

    feat: checkss in progress

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -3,21 +3,47 @@ pragma solidity 0.8.4;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./YourToken.sol";
+import "hardhat/console.sol";
 
 contract Vendor is Ownable {
+    event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
 
-  //event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+    YourToken public yourToken;
 
-  YourToken public yourToken;
+    constructor(address tokenAddress) {
+        yourToken = YourToken(tokenAddress);
+    }
 
-  constructor(address tokenAddress) {
-    yourToken = YourToken(tokenAddress);
-  }
+    // conversion rate 1 ETH = 100 ERC-20 tokens
+    uint256 public constant tokensPerEth = 100;
 
-  // ToDo: create a payable buyTokens() function:
+    /**
+     * @notice Allow users to buy tokens for ETH
+     */
+    function buyTokens() public payable {
+        require(msg.value >= 0, "Send ether to buy tokens");
 
-  // ToDo: create a withdraw() function that lets the owner withdraw ETH
+        uint256 amountOfTokens = msg.value * tokensPerEth;
 
-  // ToDo: create a sellTokens() function:
+        uint256 tokenBalance = yourToken.balanceOf(address(this));
+        require(tokenBalance >= amountOfTokens, "Not enough tokens to sell");
 
+        bool sent = yourToken.transfer(msg.sender, amountOfTokens);
+        require(sent, "Could not transfer tokens");
+
+        emit BuyTokens(msg.sender, msg.value, amountOfTokens);
+    }
+
+    /**
+     * @notice Allow the owner of the contract to withdraw ETH
+     */
+    function withdraw() public onlyOwner {
+        uint256 ownerBalance = address(this).balance;
+        require(ownerBalance > 0, "Owner has no balance to withdraw");
+
+        (bool sent, ) = msg.sender.call{value: address(this).balance}("");
+        require(sent, "Failed to send user balance back to the owner");
+    }
+
+    // ToDo: create a sellTokens() function:
 }
--- packages/hardhat/contracts/YourToken.sol
+++ packages/hardhat/contracts/YourToken.sol
@@ -2,11 +2,13 @@ pragma solidity 0.8.4;
 // SPDX-License-Identifier: MIT
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "hardhat/console.sol";
 
 // learn more: https://docs.openzeppelin.com/contracts/3.x/erc20
 
 contract YourToken is ERC20 {
     constructor() ERC20("Felix", "FLX") {
-        _mint(0xe2f9a662DE63a5AB99F937fC82A93381262740c3, 1000 * 10**18);
+        _mint(msg.sender, 1000 * 10**18);
+        console.log("Minted 1000 Felix Token to %s", msg.sender);
     }
 }
